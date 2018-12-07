6e35d780d0715c249ee315f174fb536500506fb7
acobyeal <acobyeal@naver.com>
2018-12-07 18:43:18 +0900

    ...Change

--- mysite/settings.py
+++ mysite/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'bookmark.apps.BookmarkConfig',     # (ch02) bookmark 앱을 추가 등록
     'blog.apps.BlogConfig',             # (ch03) blog 앱을 추가 등록
+    'notice.apps.NoticeConfig'          # notice(공지사항)앱 추가 등록
 ]
 
 MIDDLEWARE = [
--- mysite/urls.py
+++ mysite/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     url(r'^$', HomeView.as_view(), name='home'),  # 추가!!!
     url(r'^bookmark/', include('bookmark.urls', namespace='bookmark')),
     url(r'^blog/', include('blog.urls', namespace='blog')),
+    url(r'^notice/', include('notice.urls', namespace='notice')),
+    # notice.urls를 적용하고, 이름공간을 'notice'로 지정
 ]
